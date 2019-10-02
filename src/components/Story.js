6bcfdb0a616172f7497c1ba7412637a97306105a
DES2048 <electrochange@mail.ru>
2019-10-02 22:50:00 +0300

    Styling Story

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,6 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {getStory} from '../services/hnApi';
-import '../styles/Story.css';
+import 
+    {StoryWrapper, 
+    StoryTitle,
+    StoryMeta,
+    StoryMetaElement} from '../styles/StoryStyle';
 
 export const Story = (props) => {
 
@@ -11,14 +15,19 @@ export const Story = (props) => {
     }, []);
 
     return (
-        <div>
-            <h3 className="story-title">
+        <StoryWrapper data-testid="story">
+            <StoryTitle>
                 <a href={story.url}>{story.title}</a>
-            </h3>
-            <div>
-                <span className="by">By: <b>{story.by}</b></span>
-                <span className='at'>At: <b>{new Date(+(story.time)).toLocaleString()}</b></span>
-            </div>
-        </div>
+            </StoryTitle>
+
+            <StoryMeta>
+                <span data-testid="story-by">
+                    <StoryMetaElement color="#000">By: </StoryMetaElement>{story.by}
+                </span>
+                <span data-testid="story-time">
+                    <StoryMetaElement color="#000">Posted: </StoryMetaElement>{story.time}
+                </span>
+            </StoryMeta>
+        </StoryWrapper>
     );
 }
